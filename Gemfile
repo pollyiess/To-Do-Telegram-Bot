7edc258891422b8a0b712260d4c41b22b457d06a
Polina <dyatlowskaya.polina@yandex.ru>
2026-04-22 17:04:18 +0300

    Настроила структуру проекта

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'dotenv'
+gem 'sequel'
+gem 'sqlite3'
+gem 'telegram-bot-ruby'
+
+group :development, :test do
+  gem 'rspec'
+  gem 'rubocop'
+end
+
+group :test do
+  gem 'webmock'
+end
---
+++ lib/database.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'sequel'
+require 'sqlite3'
+
+# Класс для управления базой данных SQLite и хранения состояний пользователей
+class Database
+  def initialize
+    @db = Sequel.sqlite('db/todo_bot.sqlite3')
+    setup_tables
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def setup_tables
+    # Таблица для задач
+    @db.create_table? :tasks do
+      primary_key :id
+      Integer :user_id
+      String :title, null: false
+      String :priority, default: 'Medium'
+      TrueClass :completed, default: false
+      DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    # Таблица для состояний пользователя
+    @db.create_table? :users do
+      primary_key :id
+      Integer :telegram_id, unique: true
+      String :state, default: 'START'
+    end
+  end
+
+  # Метод для смены состояния
+  def set_state(telegram_id, state)
+    if @db[:users].first(telegram_id: telegram_id)
+      @db[:users].where(telegram_id: telegram_id).update(state: state)
+    else
+      @db[:users].insert(telegram_id: telegram_id, state: state)
+    end
+  end
+
+  # Метод для получения текущего состояния
+  def get_state(telegram_id)
+    user = @db[:users].first(telegram_id: telegram_id)
+    user ? user[:state] : 'START'
+  end
+end
